25e0946b606505694dac777b773cb8f77e409dc8
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-26 19:42:00 -0300

    test: req 2, test product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,20 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        "1",
+        "Product Name",
+        "Company Name",
+        "01/01/2023",
+        "01/01/2024",
+        "123456",
+        "Storage instructions",
+    )
+
+    assert product.__str__() == (
+        "The product 1 - Product Name with serial number 123456 "
+        "manufactured on 01/01/2023 by the company Company Name "
+        "valid until 01/01/2024 must be stored according to the "
+        "following instructions: Storage instructions."
+    )
