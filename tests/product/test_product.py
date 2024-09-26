599cdf49edd389bd06ea0bc10d855eb094fa0dc4
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-26 19:34:47 -0300

    test: req 1, test create product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        "1",
+        "Product Name",
+        "Company Name",
+        "01/01/2023",
+        "01/01/2024",
+        "123456",
+        "Storage instructions",
+    )
+
+    assert product.id == "1"
+    assert product.product_name == "Product Name"
+    assert product.company_name == "Company Name"
+    assert product.manufacturing_date == "01/01/2023"
+    assert product.expiration_date == "01/01/2024"
+    assert product.serial_number == "123456"
+    assert product.storage_instructions == "Storage instructions"
