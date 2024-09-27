efaff48178352b2a9bc9b59822b7cba331c78f2a
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-26 23:42:28 -0300

    feat: req 8, create CsvImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import csv
 from typing import Dict, Type
 
 from inventory_report.product import Product
@@ -20,16 +21,24 @@ class JsonImporter(Importer):
             return [Product(**product) for product in json.loads(file.read())]
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path, "r") as file:
+            return [
+                Product(
+                    id=product["id"],
+                    product_name=product["product_name"],
+                    company_name=product["company_name"],
+                    manufacturing_date=product["manufacturing_date"],
+                    expiration_date=product["expiration_date"],
+                    serial_number=product["serial_number"],
+                    storage_instructions=product["storage_instructions"],
+                )
+                for product in csv.DictReader(file)
+            ]
 
-
-# Não altere a variável abaixo
 
 IMPORTERS: Dict[str, Type[Importer]] = {
     "json": JsonImporter,
     "csv": CsvImporter,
 }
-if __name__ == "__main__":
-    json_importer = JsonImporter("tests/mocks/inventory.json")
-    print(json_importer.import_data())
