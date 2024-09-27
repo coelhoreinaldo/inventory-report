0cddf79ce2fc6ae29f9b0712865cce855f94b6dd
coelhoreinaldo <scoelhor7@gmail.com>
2024-09-26 23:54:12 -0300

    feat: req 9, add CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,24 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(self):
+        simple_report = super().generate()
+        all_products = [
+            product
+            for inventory in self.inventories
+            for product in inventory.data
+        ]
+        company_counter = Counter(
+            [product.company_name for product in all_products]
+        )
+        stocked_products_by_company = [
+            f"- {company}: {count}\n"
+            for company, count in company_counter.items()
+        ]
+        return (
+            simple_report
+            + "\nStocked products by company:\n"
+            + "\n".join(stocked_products_by_company)
+        )
